9d730d9a3c53366744d2adc74fc60d2b7092cc64
Vitaliy Drugak <vitosdrugak92@gmail.com>
2015-02-24 13:44:09 +0200

    add refresh button

--- src/card.js
+++ src/card.js
@@ -2,40 +2,49 @@
  * Created by vitaliydrugak on 10.02.15.
  */
 ;(function (){
+    'use strict'
+
     var setings = {
         document: document.currentScript.ownerDocument,
         myCardProto: Object.create(HTMLElement.prototype),
-        url: 'http://api.randomuser.me/'
+        url: 'http://api.randomuser.me/',
+        usr: undefined,
+        refreshButton: undefined
     }
 
 
     var API = {
-        getUser: function () {
-            var that = this,
-                xhr = new XMLHttpRequest();
 
+        getUser: function () {
+
+            var xhr = new XMLHttpRequest();
             xhr.open('GET', setings.url);
+
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === xhr.DONE) {
                     status = xhr.status;
 
                     if ((status >= 200 && status < 300) || status === 304 || status === 0) {
-                        response = JSON.parse(xhr.response || xhr.responseText);
-                        that.fillUser(response);
+                        var response = JSON.parse(xhr.response || xhr.responseText);
+                        API.fillUser(response);
                     }
                 }
             };
             xhr.send();
+
         },
+
         fillUser: function (randomUser){
-            var usr = setings.myCardProto.shadow,
-                userNode = {
-                    name: usr.querySelector('#name'),
-                    email: usr.querySelector('#email'),
-                    adress: usr.querySelector('#adress'),
-                    phone: usr.querySelector('#phone'),
-                    photo: usr.querySelector('#photo')
+            setings.usr = setings.myCardProto.shadow;
+
+            var userNode = {
+                    name: setings.usr.querySelector('#name'),
+                    email: setings.usr.querySelector('#email'),
+                    adress: setings.usr.querySelector('#adress'),
+                    phone: setings.usr.querySelector('#phone'),
+                    photo: setings.usr.querySelector('#photo')
                 },
+
                 userObj = randomUser.results[0].user;
 
             userNode.email.textContent = userObj.email;
@@ -43,7 +52,18 @@
             userNode.adress.textContent = userObj.location.state;
             userNode.phone.textContent = userObj.phone;
             userNode.photo.src = userObj.picture.medium;
+
+            if (!setings.refreshButton) {
+                init();
+            }
+        },
+
+        refresh: function () {
+            setings.refreshButton.addEventListener('click', function () {
+                API.getUser();
+            })
         }
+
     };
 
 
@@ -51,14 +71,20 @@
         createdCallback: setings.myCardProto.createdCallback = function (){
             var template = setings.document.querySelector('#random-user'),
                 usr = template.content.cloneNode(true);
+
             setings.myCardProto.shadow = this.createShadowRoot();
             setings.myCardProto.shadow.appendChild(usr);
         },
         attachedCallback: setings.myCardProto.attachedCallback = function () {
             API.getUser();
         }
-    }
+    };
+
+    function init () {
+        setings.refreshButton = setings.usr.querySelector('#refresh');
+        API.refresh();
+    };
 
 
-    var Xgithub = document.registerElement('random-user', {'prototype': setings.myCardProto});
+    document.registerElement('random-user', {'prototype': setings.myCardProto});
 }());
